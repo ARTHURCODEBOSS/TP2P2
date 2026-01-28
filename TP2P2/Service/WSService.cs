e6af28755c8d87c49029d414969d105089720ce9
Ewan-Ponchaud <155959234+Ewan-Ponchaud@users.noreply.github.com>
2026-01-28 17:29:27 +0100

    Ajoutez des fichiers projet.

---
+++ TP2P2/Service/WSService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using TP2P1.Models.EntityFramework;
+
+namespace TP2P2.Service
+{
+    public class WSService
+    {
+        private HttpClient client;
+
+        public HttpClient Client
+        {
+            get { return client; }
+            set { client = value; }
+        }
+
+        public WSService()
+        {
+            Client = new HttpClient();
+            Client = new HttpClient();
+            Client.BaseAddress = new Uri("https://convertisseurlopes-effgfwfuh2hfbdde.francecentral-01.azurewebsites.net/api/");
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        public async Task<List<Serie>> GetSeriesAsync(string nomControleur)
+        {
+            try
+            {
+                return await Client.GetFromJsonAsync<List<Serie>>(nomControleur);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ TP2P2/ViewsModels/ModelPageSerie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP2P1.Models.EntityFramework;
+using TP2P2.Service;
+
+namespace TP2P2.ViewsModels
+{
+    public class ModelPageSerie
+    {
+
+        public ModelPageSerie()
+        {
+			Service = new WSService();
+            Series = new ObservableCollection<Serie> (Service.GetSeriesAsync("serie").Result);
+        }
+
+        private ObservableCollection<Serie> series;
+
+		public ObservableCollection<Serie> Series
+        {
+			get { return series; }
+			set { series = value; }
+		}
+
+		private WSService service;
+
+		public WSService Service
+        {
+			get { return service; }
+			set { service = value; }
+		}
+
+
+	}
+}
